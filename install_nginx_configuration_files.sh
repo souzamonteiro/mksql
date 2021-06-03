32972c127c22438d58184f9df9deab66776cf5b3
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2021-06-03 19:14:07 -0300

    Added nginx MaiaMeet configuration file

--- install_nginx_configuration_files.sh
+++ install_nginx_configuration_files.sh
@@ -4,6 +4,9 @@ echo "Run this script using sudo and into maia-site directory."
 
 cp nginx/sites-available/* /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/www.maiascript.com.conf /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/meet.maiascript.com.conf /etc/nginx/sites-enabled/
 
 /etc/init.d/nginx stop
 /etc/init.d/nginx start
+
+#sudo certbot renew --dry-run
